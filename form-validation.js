30ed3e95baa70aa5862ffbff750009e097223755
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-10-10 17:45:34 -0700

    Fixed username and password submission to work correctly with green coloring showing correct and making sure that the username must be 6 character and password to be not "password".

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,23 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  if ( userEntered.length >= 6 ){
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else {
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
 
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if ( passEntered != "password" ){
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
+  else {
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
 }
