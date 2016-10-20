2f554d07ad6647ad4e3559fd366bc3be5040788e
Kyle Rodgers <krodgers4@mail.csuchico.edu>
2016-10-19 18:18:33 -0700

    Fixed colors for error and success and added an alert

--- form-validation.js
+++ form-validation.js
@@ -1,51 +1,55 @@
-function validate(){
-  //Grab the user's input and store in variables
+function register(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-
-  if ( userEntered.length >= 6 ){
-    document.getElementById("usernameGroup").classList.add("has-success");
-  }
-  else {
+  window.alert("username: "+userEntered + "\n" + "password: " + passEntered );
+}
+function validateUsername(){
+  var userEntered = document.getElementById("user").value;
+  if ( userEntered.length < 6 ) {
     document.getElementById("usernameError").innerHTML="Username must be at least 6 characters.";
     document.getElementById("usernameError").classList.remove("hidden-message");
     document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.remove("has-success");
     document.getElementById("usernameGroup").classList.add("has-error");
   }
-  if ( userEntered.search(" ") == -1 ){
-    document.getElementById("usernameGroup").classList.add("has-success");
-  }
-  else {
+  else if ( userEntered.search(" ") != -1 ) {
     document.getElementById("usernameError").innerHTML="Username can not contain any spaces.";
     document.getElementById("usernameError").classList.remove("hidden-message");
     document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.remove("has-success");
     document.getElementById("usernameGroup").classList.add("has-error");
   }
-  if ( passEntered.toLowerCase(passEntered) != "password" ){
-    document.getElementById("passwordGroup").classList.add("has-success");
-  }
   else {
+    document.getElementById("usernameGroup").classlist.remove("has-error");
+    document.getElementById("usernameGroup").classlist.add("has-success");
+  }
+}
+function validatePassword(){
+  var passEntered = document.getElementById("pass").value;
+  var userEntered = document.getElementById("user").value;
+  if ( passEntered.toLowerCase(passEntered) != "password" ) {
     document.getElementById("passwordError").innerHTML="Password cannot be password.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.remove("has-success");
     document.getElementById("passwordGroup").classList.add("has-error");
   }
-  if ( passEntered != userEntered ){
-    document.getElementById("passwordGroup").classList.add("has-success");
-  }
-  else {
+  else if ( passEntered != userEntered ) {
     document.getElementById("passwordError").innerHTML="Password can not be the same as the Username.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.remove("has-success");
     document.getElementById("passwordGroup").classList.add("has-error");
   }
-  if ( passEntered.length >= 6 || passEntered <= 20 ){
-    document.getElementById("passwordGroup").classList.add("has-success");
-  }
-  else {
+  else if ( passEntered.length < 6 || passEntered > 20 ) {
     document.getElementById("passwordError").innerHTML="Password must be between 6 and 20 characters.";
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.remove("has-success");
     document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else {
+    document.getElementById("passwordGroup").classlist.remove("has-error");
+    document.getElementById("passwordGroup").classlist.add("has-success");
   }
 }
